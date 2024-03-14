bdc6ebd53bd602f2709b11a883fc3c396c9ab7ea
Syed Ali Rezvani Rad <s.alirez1999@gmail.com>
2024-03-15 03:13:23 +0330

    removed GH actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/store-challenge/',
 	plugins: [react()],
 	test: {
 		globals: true,
